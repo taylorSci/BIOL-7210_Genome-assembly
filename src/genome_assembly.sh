89aef59d1882e3a56edbd8dcee5ae0bc92c7eb09
Nikesh Kumar <kmnike@ipsec-172-16-84-59.vpn.gatech.edu>
2021-02-23 01:40:48 -0500

    pipeline skeleton with skesa contig and bam generation (skesa and spades only) plugged in

--- src/genome_assembly.sh
+++ src/genome_assembly.sh
@@ -33,19 +33,23 @@ OPTIONS
 	-i 	install pipeline
 	-o 	[OUTPUT_FOLDER] (defaults to sibling ('../output') of input reads directory)
 	-t 	[TOOLS_FOLDER] Directory where pipeline tools will be installed (defaults to sibling ('../tools') of input reads directory)
+	-c	[NUMER_OF_CORES] Number of cores that will be used to run the pipeline
+	-m	[MEMORY] Amount of memory to allocate to the pipeline
 "
 
 # Parse user command line arguments  # TODO add assembly parameters
 install_=false
 outputDir=""
 toolsDir=""
-while getopts "hi:o:t:" option
+while getopts "hi:o:t:c:m:" option
 do
 	case $option in
 		h) 	echo $helpMessage;;
 		i) 	install_=true;;
 		o) 	outputDir=$OPTARG;;
 		t) 	toolsDir=$OPTARG;;
+		c) 	cores=$OPTARG;;
+		m) 	mem=$OPTARG;;
 		*) 	echo "UNKNOWN OPTION $OPTARG PROVIDED"
 			exit;;
 	esac
@@ -107,9 +111,44 @@ echo "Assembling with ABySS..."
 
 
 echo "Assembling with SKESA..."
+#TAYLOR, I AM MAKING THE HARD LINK TO SKESA HIDDEN. PLEASE FEEL FREE TO CHANGE THIS IF IT SHOULD BE UNHIDDEN.
+ln $inputDir/$toolsDir/skesa/SKESA/skesa .run_skesa
+mkdir $outputDir/assemblies/skesa/
+skesa_in=($(ls $outputDir/read-QC/fastp))
 
+for i in "${skesa_in[@]}";
+do
+    ./.run_skesa --reads $outputDir/read-QC/fastp/$i/${i}_1_fp.fq.gz,$outputDir/read-QC/fastp/$i/${i}_2_fp.fq.gz --cores $cores -- memory $mem > $outputDir/assemblies/skesa/$i.skesa.fa
+done
+echo "Contigs generated for SKESA"
 
 echo "Assembling with SPAdes..."
+
+echo "Generating BAM files from ABySS, SKESA, and SPAdes contigs"
+# TAYLOR, I'VE IMPLEMENTED THE BAM FILE GENERATION FOR SPADES AND SKESA AND HAVE TESTED IT. I NEED TO TALK TO YOU ABOUT HOW TO DO THIS FOR ABYSS (DEADLY WHITESPACES LOL)
+# I'VE ALSO EDITED THE SCRIPT LAYOUT WORD DOC SLIGHTLY TO INCLUDE A "BAM_FILES" DIRECTORY IN OUTPUTS WHERE YOU CAN GRAB EVERYTHING FROM
+# ALSO, I'VE ADDED -C AND -M FLAGS FOR CORES AND MEMORY INPUTS
+mkdir temp
+cd temp
+mkdir $outputDir/bam_files
+
+file_prefix=($(ls $outputDir/read-QC/fastp))
+
+for i in "${file_prefix[@]}";
+do
+    bwa index $outputDir/assemblies/skesa/${i}.skesa.fa
+    bwa mem $outputDir/assemblies/skesa/${i}.skesa.fa $outputDir/read-QC/fastp/$i/${i}_1_fp.fq.gz $outputDir/read-QC/fastp/$i/${i}_2_fp.fq.gz > ./${i}.skesa.sam
+    samtools fixmate -O bam ./${i}.skesa.sam ./${i}.skesa.bam
+    samtools sort -O bam -o $outputDir/bam_files/${i}_sorted.skesa.bam -T temp ./${i}.skesa.sam
+
+    bwa index $outputDir/assemblies/SPAdes/contigs/${i}_SPAdes.fasta
+    bwa mem $outputDir/assemblies/SPAdes/contigs/${i}_SPAdes.fasta $outputDir/read-QC/fastp/$i/${i}_1_fp.fq.gz $outputDir/read-QC/fastp/$i/${i}_2_fp.fq.gz > ./${i}_SPAdes.sam
+    samtools fixmate -O bam ./${i}_SPAdes.sam ./${i}_SPAdes.bam
+    samtools sort -O bam -o $outputDir/bam_files/${i}_sorted.SPAdes.bam -T temp ./${i}_SPAdes.sam
+done
+
+cd ..
+rm -r temp
 
 
 # Reconcile assemblies
