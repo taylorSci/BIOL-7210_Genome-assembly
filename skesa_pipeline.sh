c1abe099307f6ac5c9c6f3dbe5ba418dc8eac2cd
Nikesh Kumar <kmnike@ipsec-172-16-87-75.vpn.gatech.edu>
2021-02-24 00:23:09 -0500

    changed some output filenames to maintain consistency with other assembler outputs

--- skesa_pipeline.sh
+++ skesa_pipeline.sh
@@ -8,11 +8,12 @@ outputDir=($(dirname $inputDir)/output)
 
 #hard link no longer needed since skesa will be installed usign conda
 #ln $inputDir/toolsDir/skesa/SKESA/skesa .run_skesa
-mkdir $outputDir/assemblies/skesa/
+mkdir $outputDir/assemblies/SKESA/
+mkdir $outputDir/assemblies/SKESA/extra
 
 skesa_in=($(ls $outputDir/read_QC/fastp))
 
 for i in "${skesa_in[@]}";
 do
-    skesa --reads $outputDir/read_QC/fastp/$i/${i}_1_fp.fq.gz,$outputDir/read_QC/fastp/$i/${i}_2_fp.fq.gz --cores $c -- memory $m > $outputDir/assemblies/skesa/$i.skesa.fa
+    skesa --reads $outputDir/read_QC/fastp/$i/${i}_1_fp.fq.gz,$outputDir/read_QC/fastp/$i/${i}_2_fp.fq.gz --cores $c -- memory $m > $outputDir/assemblies/SKESA/${i}_SKESA.fasta
 done
