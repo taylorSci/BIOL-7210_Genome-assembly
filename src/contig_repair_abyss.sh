90bb9d31738701dd438970ca8249bd02f5aff071
taylorSci <taylor.b.hoyt@gmail.com>
2021-02-27 12:48:22 -0500

    Organized scrap source files into side branch.

--- src/contig_repair_abyss.sh
+++
@@ -1,9 +0,0 @@
-#!/bash/bin
-
-mkdir $outputDir/assemblies/ABySS/contigs_reapr
-ls $outputDir/assemblies/ABYSS/*.fasta > $outputDir/assemblies/ABySS/contigs_reapr/contigs.txt
-cd $outputDir/assemblies/ABySS/contigs_reapr
-
-cat contigs.txt | while read line
-do reapr facheck $line $line_repaired_ABySS.fasta
-done
